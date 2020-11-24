c7d8776b614c96e7ab52f6f1e57c3665a497f7e4
“hongchao1117git config --global user.email“497450115@qq.com <497450115@qq.com>
2020-11-24 21:07:52 +0800

    新增题目

---
+++ src/Array/findDisappearedNumbers.java
@@ -0,0 +1,19 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class findDisappearedNumbers {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[Math.abs(nums[i])-1] = -Math.abs(nums[Math.abs(nums[i])-1]);
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                res.add(i+1);
+            }
+        }
+        return res;
+    }
+}
