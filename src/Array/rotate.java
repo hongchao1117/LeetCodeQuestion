4fdb73fab8a6dcc0ecceec1e18759e487864f443
“hongchao1117git config --global user.email“497450115@qq.com <497450115@qq.com>
2021-06-01 22:22:19 +0800

    mod:题解修改

--- src/Array/rotate.java
+++ src/Array/rotate.java
@@ -3,27 +3,28 @@ package Array;
 public class rotate {
     public static void main(String[] args) {
         int[][] matrix = {
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
         };
         rotate(matrix);
         System.out.println(matrix);
     }
+
     public static void rotate(int[][] matrix) {
         int len = matrix.length;
-        for (int i = 0; i < len; i++) {
-            for (int j = i; j < len; j++) {
-                int temp = matrix[j][i];
-                matrix[j][i] = matrix[i][j];
-                matrix[i][j] = temp;
+        for (int i = 0; i < len / 2; i++) {
+            for (int j = 0; j < len; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[len - 1 - i][j];
+                matrix[len - 1 - i][j] = temp;
             }
         }
         for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len / 2; j++) {
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[i][len - j - 1];
-                matrix[i][len - j - 1] = tmp;
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
     }
--- src/LinkedList/reverseKGroup.java
+++ src/LinkedList/reverseKGroup.java
@@ -2,6 +2,24 @@ package LinkedList;
 
 public class reverseKGroup {
     public ListNode reverseKGroup(ListNode head, int k) {
-
+        ListNode dummyNode = new ListNode(-1), pre = dummyNode, cur = head, next;
+        dummyNode.next = head;
+        int len = 0;
+        while (head != null) {
+            head = head.next;
+            len++;
+        }
+        for (int i = 0; i < len / k; i++) {
+            for (int j = 0; j < k - 1; j++) {
+                next = cur.next;
+                cur.next = next.next;
+                next.next = pre.next;
+                pre.next = next;
+            }
+            pre = cur;
+            cur = pre.next;
+        }
+        return dummyNode.next;
     }
 }
+
