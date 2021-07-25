94adfa9c067444e8be206a96615c123221a05025
hongchao <497450115@qq.com>
2021-07-25 20:56:34 +0800

    mod:fix error && add some questions

---
+++ src/Stack/calculate.java
@@ -0,0 +1,33 @@
+package Stack;
+
+import java.util.Stack;
+
+public class calculate {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+        // sign 代表正负
+        int sign = 1, res = 0;
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                int cur = ch - '0';
+                while (i + 1 < length && Character.isDigit(s.charAt(i + 1)))
+                    cur = cur * 10 + s.charAt(++i) - '0';
+                res = res + sign * cur;
+            } else if (ch == '+') {
+                sign = 1;
+            } else if (ch == '-') {
+                sign = -1;
+            } else if (ch == '(') {
+                stack.push(res);
+                res = 0;
+                stack.push(sign);
+                sign = 1;
+            } else if (ch == ')') {
+                res = stack.pop() * res + stack.pop();
+            }
+        }
+        return res;
+    }
+}
