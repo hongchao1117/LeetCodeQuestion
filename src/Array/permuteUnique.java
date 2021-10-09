1c6bdd6753b322f5d4697fc91d7c6bbf62f7bb90
hongchao <497450115@qq.com>
2021-10-09 19:51:56 +0800

    add：fix

--- src/Array/permuteUnique.java
+++ src/Array/permuteUnique.java
@@ -4,13 +4,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+ */
 public class permuteUnique {
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length <= 0) return res;
-        int[] visited = new int[nums.length];
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
         Arrays.sort(nums);
-        getRes(res, nums, visited, new ArrayList<>());
+        int[] visited = new int[nums.length];
+        getRes(res, nums, visited, new ArrayList<Integer>());
         return res;
     }
 
@@ -28,7 +33,6 @@ public class permuteUnique {
             getRes(res, nums, visited, list);
             visited[i] = 0;
             list.remove(list.size() - 1);
-
         }
     }
 
