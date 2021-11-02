5704852b7757ff2cc44fdd5df1e9601e11c950bc
hongchao <497450115@qq.com>
2021-11-02 20:14:56 +0800

    add：fix

--- src/Sort/quickSort.java
+++ src/Sort/quickSort.java
@@ -2,26 +2,25 @@ package Sort;
 
 public class quickSort {
     public void quickSorting(int[] nums, int start, int end) {
+        if (start >= end) return;
         int left = start, right = end;
-        if (left < right) {
-            int standard = nums[left];
-            while (left < right) {
-                while (left < right && nums[right] >= standard) {
-                    right--;
-                }
-                if (left < right) {
-                    nums[left] = nums[right];
-                }
-                while (left < right && nums[left] < standard) {
-                    left++;
-                }
-                if (left < right) {
-                    nums[right] = nums[left];
-                }
+        int standard = nums[left];
+        while (left < right) {
+            while (left < right && standard <= nums[right]) {
+                right--;
             }
-            nums[left] = standard;
-            quickSorting(nums, start, left);
-            quickSorting(nums, left + 1, end);
+            if (left < right) {
+                nums[left] = nums[right];
+            }
+            while (left < right && nums[left] < standard) {
+                left++;
+            }
+            if (left < right) {
+                nums[right] = nums[left];
+            }
         }
+        nums[left] = standard;
+        quickSorting(nums, start, left);
+        quickSorting(nums, left + 1, end);
     }
 }
--- src/Tree/buildTree.java
+++ src/Tree/buildTree.java
@@ -2,31 +2,18 @@ package Tree;
 
 public class buildTree {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-//        TreeNode root = rec(preorder,0,preorder.length-1,inorder,0,inorder.length-1);
-//        return root;
-//    }
-//
-//    private TreeNode rec(int[] preorder, int startPre, int endPre, int[] inorder, int startIn, int endIn) {
-//        if (startIn>endIn || startPre>endPre) return null;
-//        TreeNode root = new TreeNode(preorder[startPre]);
-//        for (int i = startIn; i <= endIn; i++) {
-//            if (preorder[startPre]==inorder[i]){
-//                root.left = rec(preorder,startPre+1,startPre+i-startIn,inorder,startIn,endIn-1);
-//                root.right = rec(preorder,i-startIn+startPre+1,endPre,inorder,i+1,endIn);
-//            }
-//        }
-//        return root;
-        TreeNode root = getRes(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
-        return root;
+        return getRes(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
 
-    private TreeNode getRes(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
-        if (preStart > preEnd || inStart > inEnd) return null;
-        TreeNode root = new TreeNode(preorder[preStart]);
-        for (int i = inStart; i <= inEnd; i++) {
-            if (preorder[preStart] == inorder[i]) {
-                root.left = getRes(preorder, preStart + 1, i - inStart + preStart, inorder, inStart, i - 1);
-                root.right = getRes(preorder, i - inStart + preStart + 1, preEnd, inorder, i + 1, inEnd);
+    private TreeNode getRes(int[] preorder, int preL, int preR, int[] inorder, int inL, int inR) {
+        if (preL > preR || inL > inR) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[preL]);
+        for (int i = inL; i < inorder.length; i++) {
+            if (preorder[preL] == inorder[i]) {
+                root.left = getRes(preorder, preL + 1, preL + i - inL, inorder, inL, i - 1);
+                root.right = getRes(preorder, preL + i - inL + 1, preR, inorder, i + 1, inR);
             }
         }
         return root;
